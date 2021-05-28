a2d5b4d3a09665a9cd8e16f73ca8b26f43297066
pskLana <48616148+pskLana@users.noreply.github.com>
2021-05-28 15:48:40 +0200

    Fixed clearing of the xone props

--- deer-web-ui/src/components/Panel.jsx
+++ deer-web-ui/src/components/Panel.jsx
@@ -34,19 +34,17 @@ class Panel extends React.Component {
     this.changeInput(e, p);
 
     var temp = Object.assign({}, this.props.panelData);
-    let updatedProperties = {};
+    let updatedProperties = temp.properties;
     let excludeProps = this.props.panelData.xoneProperties.filter(i => {return i !== undefined && i !== p});
     // clear all optional except the current one
     Object.keys(temp.properties).forEach(i => {
       excludeProps.forEach(exp => {
+        console.log(exp, i);
         if (exp === i){
           updatedProperties[i] = "";
-        } else {
-          updatedProperties[i] = temp.properties[i];
-        }
+        } 
       })     
     });
-
     temp.properties = updatedProperties;
     this.props.updateParentPanelData(temp, this.props.panelData.numNodeType);
   
@@ -65,8 +63,8 @@ class Panel extends React.Component {
       <div>
         <Card>
           <div className="numbers">
-            <CardTitle tag="p">{this.props.panelData.nodePath.split('/')[1]}</CardTitle>
-            <p>{this.props.panelData.nodePath}</p>
+            <CardTitle tag="p" className="nodeName">{this.props.panelData.nodePath.split('/')[1]}</CardTitle>
+            <p className="nodePath">{this.props.panelData.nodePath}</p>
           </div> 
           <CardBody>
             <p>Properties</p>
