9ae6c4120f708acd2b09974bd7c2a178ebb15110
pskLana <48616148+pskLana@users.noreply.github.com>
2021-07-11 18:40:30 +0200

    Fixed list support #17

--- deer-web-ui/src/components/Panel.jsx
+++ deer-web-ui/src/components/Panel.jsx
@@ -81,23 +81,31 @@ class Panel extends React.Component {
     }
   }
 
-  addOneMoreProperty = (p) => {
-    p = p.replace(/\d+/g, '');
-    let propsSelector = this.props.panelData.propsSelector;
-    let temp = Object.assign({}, this.props.panelData);
-    let maxCount = propsSelector.filter(i => i.nodeSelectorProp === p)[0].maxCount;
-    let preNums = propsSelector.filter(i => i.nodeSelectorProp.includes(p) && i.nodeSelectorProp.match(/\d+/g));
-    let nums = preNums.map(i => i.nodeSelectorProp.match(/\d+/g)[0]);
-    let lastNum = Math.max(...nums) + 1;
-    if (nums.length === 0){
-      lastNum = "2";
+  addOneMorePropertyBundle = (pArr) => {
+    let isMaxCount = false;
+    let maxCount = -1;
+    for (let i = 0; i < pArr.length; i++) {
+      let p = pArr[i];
+      p = p.replace(/\d+/g, '');
+      let propsSelector = this.props.panelData.propsSelector;
+      let temp = Object.assign({}, this.props.panelData);
+      maxCount = 3;//propsSelector.filter(i => i.nodeSelectorProp === p)[0].maxCount;
+      let preNums = propsSelector.filter(i => i.nodeSelectorProp.includes(p) && i.nodeSelectorProp.match(/\d+/g));
+      let nums = preNums.map(i => i.nodeSelectorProp.match(/\d+/g)[0]);
+      let lastNum = Math.max(...nums) + 1;
+      if (nums.length === 0){
+        lastNum = "2";
+      }
+      //check max count and add additionalProp
+      if(maxCount >= lastNum){
+        temp.propsSelector.push({nodeSelectorProp: p+lastNum, maxCount: maxCount});
+        temp.properties[p+lastNum] = "";
+        this.props.updateParentPanelData(temp, this.props.panelData.numNodeType);
+      } else {
+        isMaxCount = true;
+      }
     }
-    //check max count and add additionalProp
-    if(maxCount >= lastNum){
-      temp.propsSelector.push({nodeSelectorProp: p+lastNum, maxCount: maxCount});
-      temp.properties[p+lastNum] = "";
-      this.props.updateParentPanelData(temp, this.props.panelData.numNodeType);
-    } else {
+    if(isMaxCount){
       alert("maxCount: " + maxCount);
     }
   }
@@ -107,8 +115,26 @@ class Panel extends React.Component {
     let excludeProps = this.props.panelData.xoneProperties.filter(i => {return i !== undefined});
     let propsSelector = this.props.panelData.propsSelector.map(i => i.nodeSelectorProp);
     let otherProps = Object.keys(this.props.panelData.properties).filter(otherpr => !excludeProps.includes(otherpr) && !propsSelector.includes(otherpr) && otherpr !== "operation");
-    if(excludeProps.includes("selector") && this.state.showSelectorProps === false){
+    if(excludeProps.includes("selector") && this.state.showSelectorProps === false && this.props.panelData.properties["selector"].length == 0){
       propsSelector = [];
+    }
+    if(propsSelector.length){
+      const propsSelectorSorted = [];
+      let predPropPair = [];
+      propsSelector.forEach(i => {
+        let num = i.match(/\d+/g) || 1;
+        let matchedProps;
+        if(num == 1){
+          matchedProps = propsSelector.filter(p => p.match(/\d+/g) === null);
+        } else {
+          matchedProps = propsSelector.filter(p => p.includes(num));
+        }
+        if(predPropPair[0] !== matchedProps[0]){
+          propsSelectorSorted.push(matchedProps);
+        }
+        predPropPair = matchedProps;
+      })
+      propsSelector = propsSelectorSorted;
     }
     return (
       <div>
@@ -136,33 +162,35 @@ class Panel extends React.Component {
                 <label>{pr}</label>
                 <Label check>
                   <Input type="radio" checked={this.props.panelData.properties[pr].length !== 0} onChange={(e) => this.changeRadio(e,pr)} name="radio1" />
-                  <Input
+                  {pr !== "selector" ? <Input
                       placeholder={pr}
                       type="text"
                       value={this.props.panelData.properties[pr]}
                       onChange={(e) => this.changeInputWithRadio(e,pr)}
-                  ></Input>
+                  ></Input> : <div className="shiftCheckbox"></div>}
                 </Label>
             </FormGroup>))}
-            {propsSelector.map((p) => (
-              <Fragment key={p}>
-                <FormGroup>
-                  <label>{p}</label>
-                  <div className="row">
-                    <div className="col-md-9">
-                      <Input
-                        placeholder={p}
-                        type="text"
-                        value={this.props.panelData.properties[p]}
-                        onChange={(e) => this.changeInput(e,p)}
-                      ></Input>
-                    </div>
-                    <div className="col-md-3">
-                      <Button className="plusButton" onClick={() => this.addOneMoreProperty(p)} color="primary">+</Button>
-                    </div>
-                  </div>
-                </FormGroup>
-              </Fragment>
+            {propsSelector.map((s, index) => (
+              <div className="row panelSelectorRow" key={index}>
+                <div className="col-md-9">
+                  {s.map((p) => (
+                    <Fragment key={p}>
+                      <FormGroup>
+                        <label>{p}</label>                     
+                        <Input
+                          placeholder={p}
+                          type="text"
+                          value={this.props.panelData.properties[p]}
+                          onChange={(e) => this.changeInput(e,p)}
+                        ></Input>
+                      </FormGroup>
+                    </Fragment>
+                  ))}
+                </div>
+                <div className="col-md-3">
+                  <Button className="plusButton" onClick={() => this.addOneMorePropertyBundle(s)} color="primary">+</Button>
+                </div>
+              </div>
             ))}
           </CardBody>
         </Card>
